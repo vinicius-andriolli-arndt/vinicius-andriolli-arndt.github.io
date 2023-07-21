9dfaf1e023cdf93a92ffcd517077214184e11d37
vinicius-andriolli-arndt <vinici1310@gmail.com>
2023-07-21 18:49:44 -0300

    menu de compras ravin
    usar xamp

---
+++ ravin/echo_json_itens-main/server-itens.php
@@ -0,0 +1,30 @@
+<?php
+
+echo json_encode(
+	array(
+		array(
+			"id" => "cg1",
+			"categoria" => "Salgado",
+			"nome" => "Frango teriyaki com legumes salteados",
+			"descricao" => "Franguinho no caprixo!",
+			"valor" => "38.48",
+			"imagem" => "cg1.jpg"
+		),
+		array(
+			"id" => "cag1",
+			"categoria" => "Salada",
+			"nome" => "Salada Caesar com frango grelhado",
+			"descricao" => "Saladinha com mais frango feito no Viatnã",
+			"valor" => "48.11",
+			"imagem" => "cg1.jpg"
+		),
+		array(
+			"id" => "ceg1",
+			"categoria" => "Sobremesa",
+			"nome" => "Tiramisu",
+			"descricao" => "Tiramisu mais mafiano possivel",
+			"valor" => "62.54",
+			"imagem" => "cg1.jpg"
+		)
+	)
+);
---
+++ ravin/server-itens.php
@@ -0,0 +1,78 @@
+<?php
+
+echo json_encode(
+	array(
+		array(
+			"id" => "cg1",
+			"categoria" => "salgados",
+			"nome" => "Frango teriyaki com legumes salteados",
+			"descricao" => "Franguinho no caprixo!",
+			"valor" => "38.48",
+			"imagem" => "../assets/img/produtos/specials-9.jpg"
+		),
+		array(
+			"id" => "cg1",
+			"categoria" => "salgados",
+			"nome" => "Risoto de camarão com limão siciliano",
+			"descricao" => "Almoço mais potente possivel!",
+			"valor" => "38.48",
+			"imagem" => "../assets/img/produtos/specials-8.jpg"
+		),
+		array(
+			"id" => "cg1",
+			"categoria" => "salgados",
+			"nome" => "Filé mignon ao molho de vinho tinto",
+			"descricao" => "A janta de um rei!!",
+			"valor" => "38.48",
+			"imagem" => "../assets/img/produtos/specials-1.jpg"
+		),
+		array(
+			"id" => "cag1",
+			"categoria" => "saladas",
+			"nome" => "Salada Caesar com frango grelhado",
+			"descricao" => "Saladinha com mais frango feito no Viatnã",
+			"valor" => "48.11",
+			"imagem" => "../assets/img/produtos/specials-2.jpg"
+		),
+		array(
+			"id" => "cag2",
+			"categoria" => "saladas",
+			"nome" => "Salada Caprese",
+			"descricao" => "Saladinha feita no Viatnã",
+			"valor" => "48.11",
+			"imagem" => "../assets/img/produtos/specials-7.jpg"
+		),
+		array(
+			"id" => "cag3",
+			"categoria" => "saladas",
+			"nome" => "Salada de quinoa mediterrânea",
+			"descricao" => "Saladinha feita no Viatnã",
+			"valor" => "48.11",
+			"imagem" => "../assets/img/produtos/specials-3.jpg"
+		),
+		array(
+			"id" => "ceg1",
+			"categoria" => "sobremesas",
+			"nome" => "Tiramisu",
+			"descricao" => "Tiramisu mais mafiano possivel",
+			"valor" => "62.54",
+			"imagem" => "../assets/img/produtos/specials-5.jpg"
+		),
+		array(
+			"id" => "ceg1",
+			"categoria" => "sobremesas",
+			"nome" => "Crème brûlée",
+			"descricao" => "Crème brûlée insano",
+			"valor" => "62.54",
+			"imagem" => "../assets/img/produtos/specials-6.jpg"
+		),
+		array(
+			"id" => "ceg1",
+			"categoria" => "sobremesas",
+			"nome" => "Torta de maçã com sorvete de baunilha",
+			"descricao" => "Torta feita com maçãs do ceu!",
+			"valor" => "62.54",
+			"imagem" => "../assets/img/produtos/specials-4.jpg"
+		)
+	)
+);
